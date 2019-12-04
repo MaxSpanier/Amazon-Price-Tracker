8391efacd70d109ec28199b8102d7dc20dfd11f5
MaxSpanier <maxspanier17@gmail.com>
2019-12-04 16:17:15 +0100

    Updated the GUI

--- gui.py
+++ gui.py
@@ -1,48 +1,106 @@
-from worker import start_routine, change_product
+from worker import start_routine, change_product, URL
 from visualizer import visualize
+import requests
 import wx
 
-def start_gui():
+
+class HelloFrame(wx.Frame):
+    def __init__(self, *args, **kw):
+        # ensure the parent's __init__ is called
+        super(HelloFrame, self).__init__(*args, **kw)
+
+        # create a panel in the frame
+        pnl = wx.Panel(self)
+
+        # put some text with a larger bold font on it
+        st = wx.StaticText(pnl, label="Welcome to the Amazon Price Tracker")
+        font = st.GetFont()
+        font.PointSize += 10
+        font = font.Bold()
+        st.SetFont(font)
+
+        # and create a sizer to manage the layout of child widgets
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(st, wx.SizerFlags().Border(wx.TOP | wx.LEFT, 25))
+        pnl.SetSizer(sizer)
+
+        # create a menu bar
+        self.makeMenuBar()
+
+        # and a status bar
+        self.CreateStatusBar()
+        if(checkInternet() == True):
+            self.SetStatusText("Connected to Amazon")
+        else:
+            self.SetStatusText("No Connection to Amazon")
+
+
+    def makeMenuBar(self):
+        """
+        A menu bar is composed of menus, which are composed of menu items.
+        This method builds a set of menus and binds handlers to be called
+        when the menu item is selected.
+        """
+
+        # Make a file menu with Hello and Exit items
+        fileMenu = wx.Menu()
+        # The "\t..." syntax defines an accelerator key that also triggers
+        # the same event
+        helloItem = fileMenu.Append(-1, "&Hello...\tCtrl-H",
+                                    "Help string shown in status bar for this menu item")
+        fileMenu.AppendSeparator()
+        # When using a stock ID we don't need to specify the menu item's
+        # label
+        exitItem = fileMenu.Append(wx.ID_EXIT)
+
+        # Now a help menu for the about item
+        helpMenu = wx.Menu()
+        aboutItem = helpMenu.Append(wx.ID_ABOUT)
+
+        # Make the menu bar and add the two menus to it. The '&' defines
+        # that the next letter is the "mnemonic" for the menu item. On the
+        # platforms that support it those letters are underlined and can be
+        # triggered from the keyboard.
+        menuBar = wx.MenuBar()
+        menuBar.Append(fileMenu, "&File")
+        menuBar.Append(helpMenu, "&Help")
+
+        # Give the menu bar to the frame
+        self.SetMenuBar(menuBar)
+
+        # Finally, associate a handler function with the EVT_MENU event for
+        # each of the menu items. That means that when that menu item is
+        # activated then the associated handler function will be called.
+        self.Bind(wx.EVT_MENU, self.OnHello, helloItem)
+        self.Bind(wx.EVT_MENU, self.OnExit,  exitItem)
+        self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
+
+    def OnExit(self, event):
+        """Close the frame, terminating the application."""
+        self.Close(True)
+
+    def OnHello(self, event):
+        """Say hello to the user."""
+        wx.MessageBox("Hello again from wxPython")
+
+    def OnAbout(self, event):
+        """Display an About Dialog"""
+        wx.MessageBox("This is a wxPython Hello World sample",
+                      "About Hello World 2",
+                      wx.OK | wx.ICON_INFORMATION)
+
+if __name__ == '__main__':
+    # When this module is run (not imported) then create the app, the
+    # frame, show it, and start the event loop.
     app = wx.App()
-    frame = wx.Frame(None, title="Amazon Price Tracker")
-    frame.SetSize(0, 0, 600, 400)
-
-    panel = wx.Panel(frame, wx.ID_ANY)
-    start_button = wx.Button(panel, wx.ID_ANY, "Start scraping", (10, 10))
-    start_button.Bind(wx.EVT_BUTTON, onButton)
-    
-    vis_button = wx.Button(panel, wx.ID_ANY, "Visualize the Data", (10, 40))
-    
-    change_button = wx.Button(panel, wx.ID_ANY, "Change the Product", (10, 70))
-    
-    exit_button = wx.Button(panel, wx.ID_ANY, "Exit", pos=(10, 100), size=(60, 40))
-    exit_button.Bind(wx.CLOSE, exitBtn)
-
-    frame.Show()
-    frame.Centre()
+    frm = HelloFrame(None, title='Amazon Price Tracker')
+    frm.Show()
     app.MainLoop()
 
-def onButton(self):
-    print("Button pressed")
 
-def exitBtn(self, event):
-    self.Destroy()
-
-'''
-def makeMenuBar(frm):
-    fileMenu = wx.Menu()
-    helloItem = fileMenu.Append(-1, "&Hello...\tCtrl-H",
-                "Help string shown in status bar for this menu item")
-    fileMenu.AppendSeparator()
-    exitItem = fileMenu.Append(wx.ID_EXIT)
-    helpMenu = wx.Menu()
-    aboutItem = helpMenu.Append(wx.ID_ABOUT)
-
-    menuBar = wx.MenuBar()
-    menuBar.Append(fileMenu, "&File")
-    menuBar.Append(helpMenu, "&Help")
-
-    frm.SetMenuBar(menuBar)
-'''
-
-start_gui()
+def checkInternet():
+        try:
+            if requests.get(worker.URL).ok:
+                return True
+        except:
+            return False
