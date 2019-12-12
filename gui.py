52c72dacc7e4588c6a44d4c2651c247adc478944
MaxSpanier <40826608+MaxSpanier@users.noreply.github.com>
2019-12-12 11:17:29 +0100

    changed the gui language & added gui components

--- gui.py
+++ gui.py
@@ -1,106 +1,39 @@
 from worker import start_routine, change_product, URL
 from visualizer import visualize
 import requests
-import wx
+from tkinter import font
+import tkinter
+
+top = tkinter.Tk()
+top.geometry("600x400")
+top.resizable(0, 0)
+helv36 = font.Font(family='Helvetica', size=18, weight=font.BOLD)
+
+def log():
+    print("test")
+def close_window():
+    top.destroy()
+
+scrapeBtn = tkinter.Button(top, text="Start scraping", command=log, font=helv36)
+scrapeBtn.place(x = 214.5, y = 80)
+scrapeBtn.update()
+print("Scrape: ", scrapeBtn.winfo_height(), " - ", scrapeBtn.winfo_width())
+
+visBtn = tkinter.Button(top, text="Visualize the Data", command=log, font=helv36)
+visBtn.place(x = 192.5, y = 140)
+visBtn.update()
+print("Vis: ", visBtn.winfo_height(), " - ", visBtn.winfo_width())
+
+changeBtn = tkinter.Button(top, text="Change the Product", command=log, font=helv36)
+changeBtn.place(x = 180, y = 200)
+changeBtn.update()
+print("Change: ", changeBtn.winfo_height(), " - ", changeBtn.winfo_width())
+
+exitBtn = tkinter.Button(top, text="Exit", command=close_window, font=helv36)
+exitBtn.place(x = 267.5, y = 260)
+exitBtn.update()
+print("Exit: ", exitBtn.winfo_height(), " - ", exitBtn.winfo_width())
 
 
-class HelloFrame(wx.Frame):
-    def __init__(self, *args, **kw):
-        # ensure the parent's __init__ is called
-        super(HelloFrame, self).__init__(*args, **kw)
 
-        # create a panel in the frame
-        pnl = wx.Panel(self)
-
-        # put some text with a larger bold font on it
-        st = wx.StaticText(pnl, label="Welcome to the Amazon Price Tracker")
-        font = st.GetFont()
-        font.PointSize += 10
-        font = font.Bold()
-        st.SetFont(font)
-
-        # and create a sizer to manage the layout of child widgets
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(st, wx.SizerFlags().Border(wx.TOP | wx.LEFT, 25))
-        pnl.SetSizer(sizer)
-
-        # create a menu bar
-        self.makeMenuBar()
-
-        # and a status bar
-        self.CreateStatusBar()
-        if(checkInternet() == True):
-            self.SetStatusText("Connected to Amazon")
-        else:
-            self.SetStatusText("No Connection to Amazon")
-
-
-    def makeMenuBar(self):
-        """
-        A menu bar is composed of menus, which are composed of menu items.
-        This method builds a set of menus and binds handlers to be called
-        when the menu item is selected.
-        """
-
-        # Make a file menu with Hello and Exit items
-        fileMenu = wx.Menu()
-        # The "\t..." syntax defines an accelerator key that also triggers
-        # the same event
-        helloItem = fileMenu.Append(-1, "&Hello...\tCtrl-H",
-                                    "Help string shown in status bar for this menu item")
-        fileMenu.AppendSeparator()
-        # When using a stock ID we don't need to specify the menu item's
-        # label
-        exitItem = fileMenu.Append(wx.ID_EXIT)
-
-        # Now a help menu for the about item
-        helpMenu = wx.Menu()
-        aboutItem = helpMenu.Append(wx.ID_ABOUT)
-
-        # Make the menu bar and add the two menus to it. The '&' defines
-        # that the next letter is the "mnemonic" for the menu item. On the
-        # platforms that support it those letters are underlined and can be
-        # triggered from the keyboard.
-        menuBar = wx.MenuBar()
-        menuBar.Append(fileMenu, "&File")
-        menuBar.Append(helpMenu, "&Help")
-
-        # Give the menu bar to the frame
-        self.SetMenuBar(menuBar)
-
-        # Finally, associate a handler function with the EVT_MENU event for
-        # each of the menu items. That means that when that menu item is
-        # activated then the associated handler function will be called.
-        self.Bind(wx.EVT_MENU, self.OnHello, helloItem)
-        self.Bind(wx.EVT_MENU, self.OnExit,  exitItem)
-        self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
-
-    def OnExit(self, event):
-        """Close the frame, terminating the application."""
-        self.Close(True)
-
-    def OnHello(self, event):
-        """Say hello to the user."""
-        wx.MessageBox("Hello again from wxPython")
-
-    def OnAbout(self, event):
-        """Display an About Dialog"""
-        wx.MessageBox("This is a wxPython Hello World sample",
-                      "About Hello World 2",
-                      wx.OK | wx.ICON_INFORMATION)
-
-if __name__ == '__main__':
-    # When this module is run (not imported) then create the app, the
-    # frame, show it, and start the event loop.
-    app = wx.App()
-    frm = HelloFrame(None, title='Amazon Price Tracker')
-    frm.Show()
-    app.MainLoop()
-
-
-def checkInternet():
-        try:
-            if requests.get(worker.URL).ok:
-                return True
-        except:
-            return False
+top.mainloop()
