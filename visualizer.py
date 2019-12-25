7f41afb8a85de71cf5b46c6e835b9b7f4cd9bb90
MaxSpanier <maxspanier17@gmail.com>
2019-12-25 17:59:35 +0100

    added the description for the vis-method

--- visualizer.py
+++ visualizer.py
@@ -1,3 +1,11 @@
 def visualize():
     print('Coming soon...')
     return(0)
+
+
+'''
+Write all dates and prices in two txt files 
+sperated with ,
+create a point on the x axes for every date-data
+create a point on the y axes for every price-data
+'''
